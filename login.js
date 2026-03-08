e3d7a6b954ea1f1610a145b14f20f6d66ca2c23a
sabarivasanndev <sabarivasann.dev@gmail.com>
2026-03-08 11:50:54 +0530

    I create new one

---
+++ login.js
@@ -0,0 +1,16 @@
+function run()
+{
+var input = document.getElementById("input").value;
+var result = document.getElementById("result");
+var trya= document.getElementById("trya");
+if(input=="Sabari")
+{
+    result.textContent="Home";
+    result.href="index.html";
+}
+else
+{
+    trya.textContent="Sorry, Try again";
+}
+
+}
