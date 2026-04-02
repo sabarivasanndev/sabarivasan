9449cc04e4e2b66281632c63a737c4b2a32bf51b
sabarivasanndev <sabarivasann.dev@gmail.com>
2026-04-02 14:49:29 +0530

    create

--- login.js
+++ login.js
@@ -1,16 +1,11 @@
-function run()
-{
-var input = document.getElementById("input").value;
-var result = document.getElementById("result");
-var trya= document.getElementById("trya");
-if(input=="Sabari")
-{
-    result.textContent="Home";
-    result.href="home.html";
-}
-else
-{
-    trya.textContent="Sorry, Try again";
-}
+function run() {
+    let pass = document.getElementById("pass").value;
+    let trya = document.getElementById("trya");
+    if (pass === "12345") {
+        window.location.href = "home.html"
+    }
+    else {
+        trya.textContent = "Sorry try again."
+    }
 
 }
